6f39c7e22530310a379a4a3c7758b676bc95dabb
batoolalali <abdullahbatool93@gmail.com>
2023-10-31 13:15:44 +0000

    increase the timeout

--- app.js
+++ app.js
@@ -1,54 +1,20 @@
 
-let inactivityTimeout;
-const inactivityDuration = 300000; // 0.5 minute (in milliseconds)
-
-let inactiveDivEl = document.getElementById("inactive");
-let activeDivEl = document.getElementById("active");
-let ulEl = document.getElementById("letter");
 
 
+// window.addEventListener("focus", visibilitychanged);
+window.addEventListener("blur", blur);
 
-let pEl = document.createElement("p");
-activeDivEl.appendChild(pEl);
+// const visibilityDiv = document.getElementById("visibilitychange");
+const blurDiv = document.getElementById("blur");
 
-function resetInactivityTimer() {
+// function visibilitychanged() {
+//     let pEl = document.createElement("p");
+//     visibilityDiv.appendChild(pEl);
+//     pEl.textContent = `Visibility has been changed at ${new Date()}`
+// }
 
-    clearTimeout(inactivityTimeout);
-    inactivityTimeout = setTimeout(handleInactive, inactivityDuration)
-    pEl.textContent = `last activity at ${new Date()}`
-}
-
-let i=1;
-
-setInterval(printFunction, 30000)
-
-function printFunction(){
-    let liEl = document.createElement("li");
-    ulEl.appendChild(liEl);
-    liEl.textContent= `Number: ${i++}`;
-
-}
-
-
-
-function handleInactive() {
-    // User is inactive, perform the desired action (e.g., log them out)
+function blur() {
     let pEl = document.createElement("p");
-    inactiveDivEl.appendChild(pEl);
-    pEl.textContent = `was inactive at ${new Date()}`
-
+    blurDiv.appendChild(pEl);
+    pEl.textContent = `FOCUS LOST! at ${new Date()}`
 }
-
-// Reset the inactivity timer on user interaction events
-document.addEventListener("mousemove", resetInactivityTimer);
-document.addEventListener("keydown", resetInactivityTimer);
-document.addEventListener("DOMMouseScroll", resetInactivityTimer);
-document.addEventListener("mousewheel", resetInactivityTimer);
-document.addEventListener("mousedown", resetInactivityTimer);
-document.addEventListener("touchstart", resetInactivityTimer);
-document.addEventListener("touchmove", resetInactivityTimer);
-document.addEventListener("focus", resetInactivityTimer);
-
-// Start the inactivity timer
-resetInactivityTimer();
-
