912a8fb115b8b8d00002f65c7a2553a867a91af4
batoolalali <abdullahbatool93@gmail.com>
2023-11-08 10:18:33 +0000

    adding test

--- app.js
+++ app.js
@@ -1,46 +1,113 @@
 
 
+// mij VARs
+let start = new Date().getTime();
+let data = [];
+let reported = [];
+let sessionAddress = { City: 'Amman', State: 'Amman Governorate', Country: 'JO', County: '' }
+
+
+// let google =window.google 
+// let maps= window.google.maps
+
+
+
+// Parse the JSON string back into a JavaScript object
+
 
 // window.addEventListener("focus", visibilitychanged);
 // window.addEventListener("blur", blur);
 
+/**** testing *****/
+const btn = document.getElementById("btn");
+btn.addEventListener("click", handleClick);
+
+function handleClick() {
+    let obj = {
+        reported: true,
+        reportedTime: new Date(),
+        reportedBy: "B"
+    }
+
+    reported.push(obj)
+
+}
+
+
+const btnReportIt = document.getElementById("report-it");
+btnReportIt.addEventListener("click", handleClickbtnReportIt);
+
+function handleClickbtnReportIt() {
+    let obj = {
+        reported: true,
+        reportedTime: new Date(),
+        reportedBy: "L"
+    }
+
+    reported.push(obj)
+
+}
+/**** testing *****/
 
 
 
 
-// const visibilityDiv = document.getElementById("visibilitychange");
+
+// testing VARs
 const blurDiv = document.getElementById("blur");
 const stateDiv = document.getElementById("state");
 
+
+// event VARS
 let prevState = document.visibilityState;
-console.log("prev", prevState)
 
 document.onvisibilitychange = function () {
 
+    /****testing *****/
     let pEl2 = document.createElement("p");
     stateDiv.appendChild(pEl2);
     pEl2.textContent = `State is ${document.visibilityState}`
+    /**** testing *****/
 
-
-console.log("prev", prevState);
-
-    if (document.visibilityState === 'hidden' && prevState!="hidden")  {
-        // here I should send to mij
-        blur()
+    if (document.visibilityState === 'hidden' && prevState != "hidden") {
+        sessionEnd()
     }
-     prevState= document.visibilityState;
+    prevState = document.visibilityState;
 
 };
 
 
-// function visibilitychanged() {
-//     let pEl = document.createElement("p");
-//     visibilityDiv.appendChild(pEl);
-//     pEl.textContent = `Visibility has been changed at ${new Date()}`
-// }
 
-function blur() {
+
+function sessionEnd() {
+
+    /**** testing *****/
     let pEl = document.createElement("p");
     blurDiv.appendChild(pEl);
     pEl.textContent = `the session ended at: ${new Date()}`
+    /**** testing *****/
+
+
+
+    /**** mij request *****/
+    const end = new Date().getTime();
+    const totalTime = (end - start) / 1000;
+
+    let mijObj = {
+        date: new Date(),
+        reported: reported,
+        sessionAddress: sessionAddress,
+        duration: totalTime
+    }
+
+    // Convert the object to a JSON string
+    let jsonString = JSON.stringify(mijObj);
+
+    // Save the JSON string to local storage
+    localStorage.setItem('myObject', jsonString);
+
+    location.pathname = '/data.html'
+
+
+
 }
